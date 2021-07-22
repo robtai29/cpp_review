c6b6f4ce2a1c81837093c2bf4f0133dff991aaf7
Robert Tai <robtai29@gmail.com>
2021-07-22 14:59:14 -0700

    7.22.2021

--- projects/c_string_lab/CStringLab.cpp
+++ projects/c_string_lab/CStringLab.cpp
@@ -7,30 +7,68 @@ int length(const char*);
 int countConsonants(const char* str);
 int countAlnum(const char* str);
 void toUpper(char*, unsigned int);
+char* copyString(char* buffer, unsigned int length);
 
 int main(){
-    const int LENGTH = 10;
-    char str[LENGTH];
-    std::cin.getline(str, LENGTH);
-    std::cout << "You have entered: " << str << std::endl;
-    int len = length(str);
-     std::cout << len << std::endl;
-     int count = countVowels(str);
-     std::cout << count << std::endl;
-     std::cout << countAlnum(str) << std::endl;
-     toUpper(str, len);
-     std::cout << str << std::endl;
+    const int LENGTH = 100;
+    
+    bool loop = true;
+    while (loop){
+        char* ptr = nullptr;
+        char buffer[LENGTH];
+        std::cout << "-----------------------------------" << std::endl;
+        std::cout << "Your sentence will be capitalized and counted" << std::endl;
+        std::cout << "-----------------------------------" << std::endl;
+        std::cout << "Enter a sentence: ";
+        std::cin.getline(buffer, LENGTH);
+        
+        ptr = copyString(buffer, length(buffer));
+        std::cout << "\nYou Entered: " << ptr;
+        std::cout << "\nThe length is: " << length(ptr)  << std:: endl;
+
+        std::cout << "Number of vowels is: " << countVowels(ptr)  << std:: endl;
+        std::cout << "Number of consonants is: " << countConsonants(ptr)  << std:: endl;
+        std::cout << "Number of letters and numbers is: " << countAlnum(ptr)  << std:: endl;
+        toUpper(ptr, length(ptr));
+        std::cout << "Sentence in upper case: " << ptr << std::endl;
+        std::cout << "Do you want to continue?  y/n: ";
+        char choice = 'n';
+        std::cin >> choice;
+        std::cin.ignore();
+        loop = choice == 'y';
+    }
+
+}
+
+char* copyString(char* buffer, unsigned int length){
+    char* ptr = new char[length + 1];
+    for (int i = 0; i < length + 1; i++){
+        ptr[i] = buffer[i];
+    }
+    ptr[length + 1] = '\n';
+    return ptr;
 }
 
 int countConsonants(const char* str){
-    return 26 - countVowels(str);
+   int count = 0;
+    for (const char* i = str; *i != '\0'; i++){
+        if (isalpha(*i)){
+            char ch = tolower(*i);
+            if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u'){
+                count++;
+            }
+        }
+    }
+    return count;
 }
+
 
 int countVowels(const char* str){
     int len = length(str);
     int count = 0;
     for (int i = 0; i < len; i++){
-        if (str[i] == 'o' || str[i] == 'e' || str[i] == 'i' || str[i] == 'u'){
+        char ch = tolower(str[i]);
+        if (ch == 'a' ||ch == 'o' || ch == 'e' || ch == 'i' || ch == 'u'){
             count++;
         }
     }
---
+++ projects/c_string_lab/README.md
@@ -0,0 +1,3 @@
+See also 
+https://github.com/xBradley/CSCI-41/blob/master/Lab%2002%20-%20C%20String/lab_02_c_string.cpp
+
