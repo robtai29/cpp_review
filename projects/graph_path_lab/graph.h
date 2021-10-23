525608474f610a92d04aee1d00e8c882cb2b89a7
Robert Tai <robtai29@gmail.com>
2021-10-22 20:55:58 -0700

    10.22.21

--- projects/graph_path_lab/graph.h
+++ projects/graph_path_lab/graph.h
@@ -1,23 +1,113 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cstdlib>
+#include <stdexcept>
 #include <set>
+#include <cassert>
 
 template <class Item>
 class graph{
     private:
-        inline static const MAX_VERTICES = 20;
-        vector<Item> vertices;
-        vector<vector<int>> adjacencyList;
+        inline static const int MAX_VERTICES = 20;
+        std::vector<Item> vertices;
+        std::vector<std::set<int>> adjacencyList;
+
+        void dfsTraversal(std::vector<std::vector<int>>&paths, std::vector<int>path, 
+                            std::vector<bool>& seen, int start);
     public:
-        graph();
+        graph() {}
         void add_vertex(const Item& label);
         void add_edge(int from, int to);
-        void remove_edge(int from, int to);
-        std::size_t size() const;
-        bool is_edge(std::size_t, std::size_t) const;
-        Item operator[](int index);  
+        bool remove_edge(int from, int to);
+        int size() const;
+        bool is_edge(int from, int to) const;
+        Item operator[](int index);
+        void printPaths(int start);
 };
 
+    template <class Item>
+    void graph<Item>::add_vertex(const Item& label){
+        if (vertices.size() < MAX_VERTICES){
+        vertices.push_back(label);
+        adjacencyList.push_back(std::set<int>());
+        }
+    }
+
+    template <class Item>
+    void graph<Item>::add_edge(int from, int to){
+        if (to >= 0 && to < adjacencyList.size() && from >= 0 && from < adjacencyList.size()){
+            adjacencyList[from].insert(to);
+        }else{
+            throw std::invalid_argument("out of range");
+        }
+    }
+
+    template <class Item>
+    bool graph<Item>::remove_edge(int from, int to){
+        if (adjacencyList.size() > from && adjacencyList[from].count(to) != 0){
+            adjacencyList[from].erase(to);
+            return true;
+        }
+        return false;
+
+    }
+
+    template <class Item>
+    int graph<Item>::size() const{
+        
+        return vertices.size();
+    }
+
+    template <class Item>
+    bool graph<Item>::is_edge(int from, int to) const{
+         if (to >= 0 && to < adjacencyList.size() && from >= 0 && from < adjacencyList.size()){
+            return adjacencyList[from].count(to) > 0;
+        }
+        return false;
+    }
+
+    template <class Item>
+    Item graph<Item>::operator[](int index){
+        return vertices[index];
+    }
     
+    template <class Item>
+    void graph<Item>::printPaths(int start){
+        std::vector<std::vector<int>> paths;
+        std::vector<bool> seen(vertices.size(), false);
+        std::vector<int> path;
+        dfsTraversal(paths, path, seen, start);
+        
+        for (const auto& vect : paths){
+            int i;
+            for (i = 0; i < vect.size() -1; i++){
+                std::cout << vect[i] << " -> ";
+            }
+            std::cout << vect[i];
+            std::cout << std::endl;
+        }
+
+    }
+
+    template <class Item>
+    void graph<Item>::dfsTraversal(std::vector<std::vector<int>>&paths, std::vector<int>path, 
+        std::vector<bool>& seen, int start){
+            path.push_back(start);
+
+            if (adjacencyList[start].size() == 0){
+                paths.push_back(path);
+                return;
+            }
+
+            if (seen[start] == true){
+                paths.push_back(path);
+                return;
+            }
+
+            seen[start] = true;
+
+            for (auto itr = std::begin(adjacencyList[start]); itr != std::end(adjacencyList[start]); itr++){
+                dfsTraversal(paths, path, seen, *itr);
+            }
+
+        }
--- projects/graph_path_lab/main.cpp
+++ projects/graph_path_lab/main.cpp
@@ -7,5 +7,21 @@ using namespace std;
 int main()
 {
 	graph<int> graph1;
+	cout << "hello world" << endl;
+	graph1.add_vertex(0);
+	graph1.add_vertex(1);
+	graph1.add_vertex(2);
+	graph1.add_vertex(3);
+	graph1.add_vertex(4);
+	graph1.add_vertex(5);
 
+	graph1.add_edge(0, 1);
+	graph1.add_edge(1, 2);
+	graph1.add_edge(2, 3);
+	graph1.add_edge(2, 4);
+	graph1.add_edge(4, 5);
+	graph1.add_edge(5, 0);
+	graph1.add_edge(3, 1);
+
+	graph1.printPaths(0);
 }
